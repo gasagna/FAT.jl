ef6e1dcca760034a445c680981a3acf21aeb0fb4
Davide Lasagna <davide.lasagna@soton.ac.uk>
2016-04-10 17:20:02 +0100

    eliminated old todos

--- src/FAT.jl
+++ src/FAT.jl
@@ -6,11 +6,7 @@ isdefined(Base, :__precompile__) && __precompile__(true)
 module FAT
 
 # TODO
-# ~ make the Field and SimulationData aware of the format the simulation is stored 
-#   in the hdf file, by making them parametric
 # ~ move the mesh data structure to the h5 file for faster loading
-# ~ when you open a simulation you should have a command to close it
-# ~ add integrals over patches 
 
 include("constants.jl")
 include("ofio.jl")
