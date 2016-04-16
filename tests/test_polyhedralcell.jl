ac65790687b18bccd6a70b5c68c5f180ccefcd24
Davide Lasagna <davide.lasagna@soton.ac.uk>
2016-04-16 17:38:24 +0100

    fixed tests for polyhedral cell

--- tests/test_polyhedralcell.jl
+++ tests/test_polyhedralcell.jl
@@ -1,6 +1,12 @@
 using Base.Test
 using FAT.Meshes
 
+# rotation matrices
+Q1 = [1    0          0;
+      0    sqrt(3)/2  1/2;
+      0    -1/2       sqrt(3)/2]
+Q2 = eye(3, 3)      
+
 # regular cube
 ab = Point(0.0, 0.0, 0.0)
 bb = Point(1.0, 0.0, 0.0)
@@ -21,104 +27,34 @@ faceIDss = [(1, 2, 3, 4), # bottom
             (1, 5, 6, 2), # front
             (5, 6, 7, 8)] # top
 
-fcs = PolygonalFace{Float64, 4}[]
-for faceIDs in faceIDss
-    pts = ntuple(i -> nds[faceIDs[i]], 4)
-    face = PolygonalFace{Float64, 4}(faceIDs, 
-                             FAT.Meshes._centre(pts),
-                             FAT.Meshes._svec(pts),
-                             0x00000001, # this is not tested here
-                             0x00000002) # this is not tested here
-    push!(fcs, face)
+for Q in (Q1, Q2)
+
+    # rotate points
+    ab_ = Point(Q*asarray(ab)...)
+    bb_ = Point(Q*asarray(bb)...)
+    cb_ = Point(Q*asarray(cb)...)
+    db_ = Point(Q*asarray(db)...)
+    at_ = Point(Q*asarray(at)...)
+    bt_ = Point(Q*asarray(bt)...)
+    ct_ = Point(Q*asarray(ct)...)
+    dt_ = Point(Q*asarray(dt)...)
+    nds = [ab_, bb_, cb_, db_, at_, bt_, ct_, dt_]
+
+    # centres and areas of faces
+    fcentres = Point{Float64}[]
+    fareas = Float64[]
+
+    for faceIDs in faceIDss
+        pts = ntuple(i -> nds[faceIDs[i]], 4)
+        push!(fcentres, FAT.Meshes._centre(pts))
+        push!(fareas,   FAT.Meshes._area(pts))
+    end
+
+    # build cell
+    cc, cv = FAT.Meshes._centreAndVolume(tuple(fareas...), tuple(fcentres...))
+
+    # check centre and volume
+    @test distance(cc, Point(Q*asarray(Point(0.5, 0.5, 0.5))...)) < 5e-16
+    @test cv ≈ 1.0
 end
-
-# test areas are all unitary
-for face in fcs
-    @test area(face) == 1.0
-end
-
-# build cell
-cell = PolyHedralCell{Float64, 6}(ntuple(i->UInt32(i), 6),
-                FAT.Meshes._centreAndVolume(tuple(fcs...))...)
-
-# test volume
-@test volume(cell) == 1.0
-
-# test centre
-@test centre(cell) == Point(0.5, 0.5, 0.5)
-
-
-# now apply a rotation and check volume is still the same
-Q = [1    0          0;
-     0    sqrt(3)/2  1/2;
-     0    -1/2       sqrt(3)/2]
-
-ab = Point(Q*asarray(ab)...)
-bb = Point(Q*asarray(bb)...)
-cb = Point(Q*asarray(cb)...)
-db = Point(Q*asarray(db)...)
-at = Point(Q*asarray(at)...)
-bt = Point(Q*asarray(bt)...)
-ct = Point(Q*asarray(ct)...)
-dt = Point(Q*asarray(dt)...)
-nds = [ab, bb, cb, db, at, bt, ct, dt]
-
-# do the same
-fcs = PolygonalFace{Float64, 4}[]
-for faceIDs in faceIDss
-    pts = ntuple(i -> nds[faceIDs[i]], 4)
-    face = PolygonalFace{Float64, 4}(faceIDs, 
-                             FAT.Meshes._centre(pts),
-                             FAT.Meshes._svec(pts),
-                             0x000000001, 
-                             0x000000002)
-    push!(fcs, face)
-end
-
-# test areas are all unitary
-for face in fcs
-    @test area(face) ≈ 1.0
-end
-
-# build cell
-cell = PolyHedralCell{Float64, 6}(ntuple(i->UInt32(i), 6),
-                FAT.Meshes._centreAndVolume(tuple(fcs...))...)
-
-# test volume
-@test volume(cell) == 1.0
-
-# test centre
-@test distance(centre(cell), Point(Q*asarray(Point(0.5, 0.5, 0.5))...)) < 1e-15
-
-# now apply displacement and scaling to the points and verify still works
-s = Point(-1.0, 2, -4)
-nds = [2*ab-s, 2*bb-s, 2*cb-s, 2*db-s, 2*at-s, 2*bt-s, 2*ct-s, 2*dt-s]
-
-# do the same
-fcs = PolygonalFace{Float64, 4}[]
-for faceIDs in faceIDss
-    pts = ntuple(i -> nds[faceIDs[i]], 4)
-    face = PolygonalFace{Float64, 4}(faceIDs, 
-                             FAT.Meshes._centre(pts),
-                             FAT.Meshes._svec(pts),
-                             0x000000001, 
-                             0x000000002)
-    push!(fcs, face)
-end
-
-# test areas are all unitary time twice the scaling
-for face in fcs
-    @test area(face) ≈ 1.0*2*2
-end
-
-# build cell
-cell = PolyHedralCell{Float64, 6}(ntuple(i->UInt32(i), 6),
-                FAT.Meshes._centreAndVolume(tuple(fcs...))...)
-
-# test volume is thrice the scaling
-@test volume(cell) ≈ 1.0*2*2*2
-
-# test centre
-@test distance(centre(cell), 2*Point(Q*asarray(Point(0.5, 0.5, 0.5))...) - s) < 1e-15
-
 
