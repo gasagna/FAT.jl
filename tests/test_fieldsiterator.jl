3cb5635553e75e5e46f854a7ef7ebc876ca6f707
Davide Lasagna <lasagnadavide@gmail.com>
2018-08-12 14:14:47 +0100

    added tests for file loader

--- tests/test_fieldsiterator.jl
+++ tests/test_fieldsiterator.jl
@@ -1,18 +1,18 @@
 using Base.Test
 
-using FAT.Fields
-using FAT.Meshes
 using FAT.Simulation
 
-# do tests on ascii data format
-sim = SimulationData("ldc_test_ascii", (1, 2))
+for path in ("ldc_test_ascii", "ldc_test_binary")
+    # simulation object
+    sim = SimulationData(path, (1, 2))
 
-# construct iterator object for velocity skipping field at t=0
-us = vfields(sim, :U, 1:10)
+    # construct iterator object for velocity skipping field at t=0
+    us = vfields(sim, :U, 1:10)
 
-# check length
-@test length(us) == 10
+    # check length
+    @test length(us) == 10
 
-# check from data file ldc_test_ascii/1/U
-@test us[1][1].internalField[1]   == 0.000259015397
-@test us[1][2].internalField[end] == -0.147795835
+    # check from data file ldc_test_ascii/1/U
+    @test us[1][1].internalField[1] == 0.000259015397
+    @test us[1][2].internalField[end] == -0.147795835
+end
