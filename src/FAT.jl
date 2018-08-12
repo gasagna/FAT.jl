f11a7681c22c869f7a2a42caca03fba99088f4ea
Davide Lasagna <lasagnadavide@gmail.com>
2018-08-12 14:19:39 +0100

    import all modules

--- src/FAT.jl
+++ src/FAT.jl
@@ -4,20 +4,15 @@
 
 module FAT
 
-# TODO
-# ~ move the mesh data structure to the h5 file for faster loading
-
 include("constants.jl")
 include("ofio.jl")
 include("meshes.jl")
-# include("utils.jl")
 include("field.jl")
-# include("pod.jl")
+include("pod.jl")
 include("simulations.jl")
-# include("sparse.jl")
-# include("galerkin.jl")
+include("sparse.jl")
+include("galerkin.jl")
 
-using .Constants, .Meshes, .Fields, .Simulation# .OFIO, .POD, .Sparse, .Galerkin
-
+using .Constants, .Meshes, .Fields, .Simulation, .OFIO, .POD, .Sparse, .Galerkin
 
 end
